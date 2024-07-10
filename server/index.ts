726c0b9852f6771e0a3a08821895f29df7cec143
Advait Junnarkar <advait.junnarkar@daisee.com>
2024-07-10 18:14:58 +1000

    feat(server): health check. Fetch Xero balance sheet

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,23 @@
 import { app } from './utils/app';
 import { PORT } from './constants';
 
-app.get('/healthz', (req, res) => {
+// Health check
+app.get('/healthz', (_req, res) => {
   res.status(200).send();
+});
+
+app.get('/balanceSheet', async (_req, res) => {
+  try {
+    const fetchResponse = await fetch(
+      process.env.XERO_API_URL ||
+        'http://localhost:3000/api.xro/2.0/Reports/BalanceSheet'
+    );
+    const data = await fetchResponse.json();
+
+    res.status(200).send(data);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 app.listen(PORT, () => console.log(`Listening on PORT:${PORT}`));
