614bd692347f2e88fe9b93a9e756af5472d30da0
Advait Junnarkar <advait.junnarkar@daisee.com>
2024-07-11 13:23:00 +1000

    feat(server): use morgan

--- server/utils/app.ts
+++ server/utils/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import cors from 'cors';
+import morgan from 'morgan';
 
 const app = express();
 
 app.use(cors({ origin: '*' }));
 app.use(express.json());
+app.use(morgan('dev'));
 
 export { app };
